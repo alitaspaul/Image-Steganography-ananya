b69d0b74441b55bd5fcccaf3a0d97eb1e53f3ea5
Ananya Nair <113717075+ananyanair31@users.noreply.github.com>
2023-09-10 22:50:06 +0530

    Update Steganography.py

--- Steganography.py
+++ Steganography.py
@@ -5,18 +5,18 @@ class Steg:
   #Image Steganogrpahy using LSB method
   def stegnaography():
     #convert cover image to grayscale
-    img1 = Image.open("image1.jpg")
+    img1 = Image.open("coverImage.jpg")
     grey_img1 = np.array(img1.convert('L'))
 
       #convert the secret image to greyscale
-    img2 = Image.open("secret.jpg")
+    img2 = Image.open("secretImage.jpg")
     grey_img2 = np.array(img2.convert('L'))
 
     #embedded image
     new = np.zeros(grey_img2.size)
     new.shape = (256,256)
 
-      #remove 4 lsb in cover image
+    #remove 4 lsb in cover image
     for i in range(256):
       for j in range(256):
         grey_img1[i][j] = grey_img1[i][j] & 240
@@ -26,7 +26,7 @@ class Steg:
       for j in range(256):
         if len(np.binary_repr(grey_img2[i][j])) == 7:
           grey_img2[i][j] = grey_img2[i][j] >> 3
-        if len(np.binary_repr(grey_img2[i][j])) == 8:
+        elif len(np.binary_repr(grey_img2[i][j])) == 8:
           grey_img2[i][j] = grey_img2[i][j] >> 4
 
     #merging msb of cover and secret image
